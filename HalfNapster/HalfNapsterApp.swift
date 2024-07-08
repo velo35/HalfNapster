7b3344181e79ba8ed35cbaee4e976e64eed81cee
Scott Daniel <sadaniel35@gmail.com>
2024-07-08 17:13:18 -0500

    Initial project check-in and .gitignore

---
+++ HalfNapster/HalfNapsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfNapsterApp.swift
+//  HalfNapster
+//
+//  Created by Scott Daniel on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HalfNapsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
