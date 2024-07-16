3e46c450a3de478242cfa9bdb9ef05bd8eee30be
Scott Daniel <sadaniel35@gmail.com>
2024-07-16 16:37:49 -0700

    Enable multiselection of playlists in LibraryView

--- HalfNapster/Views/Library/LibraryView.swift
+++ HalfNapster/Views/Library/LibraryView.swift
@@ -12,15 +12,30 @@ struct LibraryView: View
     @Environment(OAuthService.self) private var authService
     @Environment(LibraryViewModel.self) private var viewModel
     
+    @State private var selecting = false
+    @State private var selection = Set<Playlist.ID>()
+    
     var body: some View
     {
         NavigationStack {
-            List {
-                ForEach(viewModel.playlists) { playlist in
+            List(viewModel.playlists, selection: $selection) { playlist in
+                if selecting {
+                    Text(playlist.name)
+                }
+                else {
                     NavigationLink(value: playlist) {
                         Text(playlist.name)
                     }
                 }
+            }
+            .toolbar {
+                if selecting {
+                    Button("Export") {
+                        
+                    }
+                }
+                
+                Toggle("Selection", isOn: $selecting)
             }
             .navigationDestination(for: Playlist.self) { playlist in
                 PlaylistView()
